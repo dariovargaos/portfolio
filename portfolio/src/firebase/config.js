823577d84c7bee5fb88f12fd88c358c3514bc891
dariovargaos <dario.varga2012@gmail.com>
2024-01-16 17:00:09 +0100

    Reverted changes regarding firebase config file, added new favicon icon

--- portfolio/src/firebase/config.js
+++ portfolio/src/firebase/config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: "AIzaSyB4j2cY9OZnZfYs5Cu8HM252RcpmRrWi_k",
+  authDomain: "portfolio-ad943.firebaseapp.com",
+  projectId: "portfolio-ad943",
+  storageBucket: "portfolio-ad943.appspot.com",
+  messagingSenderId: "752211603737",
+  appId: "1:752211603737:web:2bb72e2c9a888ea3e5d588",
 };
 
 //init firebase
