5e8b48c7c2542fde072b55c805b4a2aa8f6201fb
dariovargaos <dario.varga2012@gmail.com>
2024-01-18 10:36:41 +0100

    Modified download resume depending on which langage is website

--- portfolio/src/pages/resume/Resume.jsx
+++ portfolio/src/pages/resume/Resume.jsx
@@ -36,7 +36,11 @@ export default function AboutMe() {
         <Button
           leftIcon={<FaFilePdf />}
           as="a"
-          href="https://firebasestorage.googleapis.com/v0/b/portfolio-ad943.appspot.com/o/CV%20English%20-%20Dario%20Varga.pdf?alt=media&token=2b5cc7a4-bf38-4171-aa5e-15c04c068f15"
+          href={
+            language === "en"
+              ? "https://firebasestorage.googleapis.com/v0/b/portfolio-ad943.appspot.com/o/CV%20English%20-%20Dario%20Varga.pdf?alt=media&token=55409ee4-001e-4dfc-800e-69f9bf9d9849"
+              : "https://firebasestorage.googleapis.com/v0/b/portfolio-ad943.appspot.com/o/CV%20-%20Dario%20Varga.pdf?alt=media&token=378db68f-ce12-49f7-8356-64923c0e86e3"
+          }
           download="Varga_Dario_CV"
           target="blank"
         >
@@ -47,7 +51,7 @@ export default function AboutMe() {
       <Flex p={8}>
         <Flex boxShadow="md" justify="center">
           <Image
-            w={isSmallScreen ? "100%" : "70%"}
+            w={isSmallScreen ? "100%" : "60%"}
             src={
               language === "en"
                 ? "https://firebasestorage.googleapis.com/v0/b/portfolio-ad943.appspot.com/o/Professional%20CV%20Resume%20image.png?alt=media&token=5e804952-eb5e-46a5-b9b7-20f7a08ea8d2"
